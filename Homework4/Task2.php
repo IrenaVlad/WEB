1d29836ce59b84e842cf2a32906bb2904e33463d
IrenaVlad <iriscka@bk.ru>
2022-12-03 23:10:58 +0100

    add homework4

---
+++ Homework4/Task2.php
@@ -0,0 +1,14 @@
+-- create
+CREATE TABLE EMPLOYEE (
+  empId INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age TEXT NOT NULL,
+  address TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Ivan', '32', 'Shaduntsa 7');
+INSERT INTO EMPLOYEE VALUES (0002, 'Irina', '34', 'San Bernardo 57');
+INSERT INTO EMPLOYEE VALUES (0003, 'Vadim', '38', 'San Bernardo 57');
+INSERT INTO EMPLOYEE VALUES (0004, 'Sergei', '62', 'Zarechnaya 10');
+INSERT INTO EMPLOYEE VALUES (0005, 'Olga', '61', 'Zarechnaya 10');
